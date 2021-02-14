7a29add11d3053631337c4621dd17839d831c3c9
Ganesh Pagade <ganesh.pagade@zalando.de>
2021-02-14 14:24:09 +0100

    Read config from file based on env args for consumer

--- metricsconsumer/metricsconsumer.py
+++ metricsconsumer/metricsconsumer.py
@@ -1,21 +1,23 @@
 import json
 import atexit
+import configparser
+import argparse
 
 from kafka import KafkaConsumer
 from psycopg2.extras import RealDictCursor
 import psycopg2
 
-topic_name = 'website-metrics-test'
-db_name = 'metricsdb_test'
-uri = 'postgres://avnadmin:t7lshsuw02az70km@pg-3c8e9bc0-ganesh-3896.aivencloud.com:18548/' + \
-      db_name + '?sslmode=require'
 create_table_sql = 'CREATE TABLE IF NOT EXISTS website_metrics (event_time BIGINT PRIMARY KEY, metrics JSONB)'
 select_count_table_sql = 'SELECT COUNT(*) FROM website_metrics'
 insert_table_sql = 'INSERT INTO website_metrics VALUES (%s, %s) ON CONFLICT DO NOTHING'
-consumer_poll_timeout = 10000
 
 
 def setup_db():
+    db_config = config['db']
+
+    uri = 'postgres://' + db_config['user'] + ':' + db_config['password'] + '@' + db_config['host'] + ':' + \
+          db_config['port'] + '/' + db_config['db_name'] + '?sslmode=require'
+
     db_conn = psycopg2.connect(uri)
     cursor = db_conn.cursor(cursor_factory=RealDictCursor)
     cursor.execute(create_table_sql)
@@ -36,7 +38,7 @@ def get_total_metrics(db_conn):
 
 
 def consume_message(consumer, db_conn, cursor):
-    raw_msgs = consumer.poll(timeout_ms=consumer_poll_timeout)
+    raw_msgs = consumer.poll(timeout_ms=int(config['kafka']['consumer_poll_timeout']))
     for tp, messages in raw_msgs.items():
         for message in messages:
             print(message.value.decode('utf-8'))
@@ -61,18 +63,20 @@ def exit_handler(consumer, db_conn):
 
 
 def get_consumer():
+    kafka_config = config['kafka']
+
     consumer = KafkaConsumer(
-        bootstrap_servers='kafka-31d994f-ganesh-3896.aivencloud.com:18550',
-        auto_offset_reset='earliest',
-        security_protocol="SSL",
-        group_id='metrics-consumer-group',
-        ssl_cafile='secrets/ca.pem',
-        ssl_certfile='secrets/service.cert',
-        ssl_keyfile='secrets/service.key',
-        consumer_timeout_ms=1000,
+        bootstrap_servers=kafka_config['server'] + ':' + kafka_config['port'],
+        auto_offset_reset=kafka_config['auto_offset_reset'],
+        security_protocol=kafka_config['security_protocol'],
+        group_id=kafka_config['group_id'],
+        ssl_cafile=kafka_config['ssl_cafile'],
+        ssl_certfile=kafka_config['ssl_certfile'],
+        ssl_keyfile=kafka_config['ssl_keyfile'],
+        consumer_timeout_ms=int(kafka_config['consumer_timeout_ms']),
     )
 
-    consumer.subscribe([topic_name])
+    consumer.subscribe([kafka_config['topic_name']])
     return consumer
 
 
@@ -84,5 +88,20 @@ def main():
     run_consumer(consumer, db_conn)
 
 
+def get_config_file_name():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--env", "-e", help="Environment in which to run the program.",
+                        choices=['test', 'prod'],
+                        required=True)
+    args = parser.parse_args()
+
+    return 'config/config_' + args.env + '.ini'
+
+
 if __name__ == "__main__":
+    config_file_name = get_config_file_name()
+
+    config = configparser.ConfigParser()
+    config.read(config_file_name)
+
     main()
