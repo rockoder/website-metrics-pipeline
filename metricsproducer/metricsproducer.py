a0fe4bf1ea3bc6fc9b81f05ff8f28a23c47b8f86
Ganesh Pagade <ganesh.pagade@zalando.de>
2021-02-14 14:02:16 +0100

    Read config from file based on env args for producer

--- metricsproducer/metricsproducer.py
+++ metricsproducer/metricsproducer.py
@@ -2,6 +2,8 @@ import re
 import time
 import json
 import atexit
+import configparser
+import argparse
 
 import requests
 from kafka import KafkaProducer
@@ -12,22 +14,16 @@ from kafka import KafkaProducer
 # todo: exception handling and flushing on error and also on exit
 # todo: add logging
 
-topic_name = 'website-metrics-test'
-url = 'https://httpbin.org/get'
-regex_pattern = 'origin'
-pattern = re.compile(regex_pattern)
-poll_interval = 5
-time_precision = 1000
-requests_timeout = 10
-
 
 def create_message(event_time, response, regex_found):
+    website_config = config['website']
+
     message = {
         'response_code': response.status_code,
         'response_time': response.elapsed.total_seconds(),
         'regex_found': regex_found,
-        'regex_pattern': regex_pattern,
-        'url': url,
+        'regex_pattern': website_config['regex_pattern'],
+        'url': website_config['url'],
         'event_time': event_time
     }
 
@@ -43,8 +39,8 @@ def process_website_metric(response):
 
 
 def collect_website_metric():
-    event_time = int(time.time() * time_precision)
-    response = requests.get(url, timeout=requests_timeout)
+    event_time = int(time.time() * int(config['website']['time_precision']))
+    response = requests.get(config['website']['url'], timeout=int(config['website']['requests_timeout']))
 
     return event_time, response
 
@@ -58,13 +54,13 @@ def get_website_metric_message():
 
 def produce_metrics(producer):
     message_json = get_website_metric_message()
-    producer.send(topic_name, message_json)
+    producer.send(config['kafka']['topic_name'], message_json)
 
 
 def run_producer(producer):
     while True:
         produce_metrics(producer)
-        time.sleep(poll_interval)
+        time.sleep(int(config['website']['poll_interval']))
 
 
 def exit_handler(producer):
@@ -73,12 +69,14 @@ def exit_handler(producer):
 
 
 def get_producer():
+    kafka_config = config['kafka']
+
     producer = KafkaProducer(
-        bootstrap_servers='kafka-31d994f-ganesh-3896.aivencloud.com:18550',
-        security_protocol="SSL",
-        ssl_cafile='secrets/ca.pem',
-        ssl_certfile='secrets/service.cert',
-        ssl_keyfile='secrets/service.key',
+        bootstrap_servers=kafka_config['server'] + ':' + kafka_config['port'],
+        security_protocol=kafka_config['security_protocol'],
+        ssl_cafile=kafka_config['ssl_cafile'],
+        ssl_certfile=kafka_config['ssl_certfile'],
+        ssl_keyfile=kafka_config['ssl_keyfile'],
     )
 
     return producer
@@ -91,5 +89,21 @@ def main():
     run_producer(producer)
 
 
+def get_config_file_name():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--env", "-e", help="Environment in which to run the program.",
+                        choices=['test', 'prod'],
+                        required=True)
+    args = parser.parse_args()
+
+    return 'config/config_' + args.env + '.ini'
+
+
 if __name__ == "__main__":
+    config_file_name = get_config_file_name()
+
+    config = configparser.ConfigParser()
+    config.read(config_file_name)
+    pattern = re.compile(config['website']['regex_pattern'])
+
     main()
